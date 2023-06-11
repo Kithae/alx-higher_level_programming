1f22bdfabe35cb1d9c18e3d875d837f1d8542b5c
Rachel <rachelkithae@yahoo.com>
2023-06-11 12:35:16 -0700

    task 13

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,32 @@
+#include "lists.h"
+
+/**
+ * is_palindrome – a function for checking palindrome in singly linked lists
+ * @head: a pointer
+ * Return: 1 else,  0
+ */
+
+int is_palindrome(listint_t **head)
+{
+	int a = 0, b, k;
+	listint_t  *node = *head, *curr, *lst;
+	
+	if (!head)
+		return (1);
+	while (node)
+	{
+		node = node->next;
+		a++;
+	}
+	curr = *head, lst = *head;
+	for (b = 0; b < a / 2 ; b++)
+	{
+		for (k = b; k < a - 1; k++)
+			lst = lst->next;
+		if (lst->n != curr->n)
+			return (0);
+		curr = curr->next;
+		lst = *head;
+	}
+	return (1);
+}
