4f44c71265052c62198f64668e6c50e61cf0ed89
Rachel <rachelkithae@yahoo.com>
2023-06-26 12:22:09 -0700

    advanced tasks

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,14 +7,3 @@ def safe_print_integer_err(value):
     except (ValueError, TypeError) as e:
         print("Exception: {}".format(e), file=sys.stderr)
         return False
-========================================
-101-safe_function.py
-#!/usr/bin/python3
-def safe_function(fct, *args):
-    import sys
-    try:
-        a = fct(*args)
-        return a
-    except Exception as e:
-        print("Exception: {}".format(e), file=sys.stderr)
-        return None
