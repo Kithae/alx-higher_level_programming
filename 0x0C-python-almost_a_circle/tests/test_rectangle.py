c3b5d921c64065fb2f4bfe63f914aef720f86423
Rachel <rachelkithae@yahoo.com>
2023-07-17 11:45:54 -0700

    more testing

--- 0x0C-python-almost_a_circle/tests/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_rectangle.py
@@ -1,4 +1,4 @@
-    #!/usr/bin/python3
+#!/usr/bin/python3
 """model for unittests for rectangle.py.
 Unittest specified classes:
     TestRectangle_instantiation start at line 25
@@ -23,7 +23,7 @@ class TestRectangle_instantiation(unittest.TestCase):
     """Unittests for rectangle class instantiation."""
 
     def test_rectangle_is_base(self):
-        self.assertIsInstance(Rectangle(8, 4), Base)
+        self.assertIsInstance(Rectangle(10, 2), Base)
 
     def test_no_args(self):
         with self.assertRaises(TypeError):
@@ -34,44 +34,44 @@ class TestRectangle_instantiation(unittest.TestCase):
             Rectangle(1)
 
     def test_two_args(self):
-        r1 = Rectangle(8, 4)
-        r2 = Rectangle(4, 8)
+        r1 = Rectangle(10, 2)
+        r2 = Rectangle(2, 10)
         self.assertEqual(r1.id, r2.id - 1)
 
     def test_three_args(self):
-        r1 = Rectangle(5, 3, 1)
-        r2 = Rectangle(6, 5, 8)
+        r1 = Rectangle(2, 2, 4)
+        r2 = Rectangle(4, 4, 2)
         self.assertEqual(r1.id, r2.id - 1)
 
     def test_four_args(self):
-        r1 = Rectangle(3, 7, 2, 4)
-        r2 = Rectangle(8, 6, 2, 9)
+        r1 = Rectangle(1, 2, 3, 4)
+        r2 = Rectangle(4, 3, 2, 1)
         self.assertEqual(r1.id, r2.id - 1)
 
     def test_five_args(self):
-        self.assertEqual(7, Rectangle(7, 4, 0, 0, 9).id)
+        self.assertEqual(7, Rectangle(10, 2, 0, 0, 7).id)
 
     def test_more_than_five_args(self):
         with self.assertRaises(TypeError):
-            Rectangle(6, 5, 8, 4, 2, 9)
+            Rectangle(1, 2, 3, 4, 5, 6)
 
     def test_width_private(self):
         with self.assertRaises(AttributeError):
-            print(Rectangle(8, 3, 0, 0, 6).__width)
+            print(Rectangle(5, 5, 0, 0, 1).__width)
 
     def test_height_private(self):
         with self.assertRaises(AttributeError):
-            print(Rectangle(8, 3, 0, 0, 6).__height)
+            print(Rectangle(5, 5, 0, 0, 1).__height)
 
     def test_x_private(self):
         with self.assertRaises(AttributeError):
-            print(Rectangle(8, 3, 0, 0, 6).__x)
+            print(Rectangle(5, 5, 0, 0, 1).__x)
 
     def test_y_private(self):
         with self.assertRaises(AttributeError):
-            print(Rectangle(8, 3, 0, 0, 6).__y)
+            print(Rectangle(5, 5, 0, 0, 1).__y)
 
-def test_width_getter(self):
+    def test_width_getter(self):
         r = Rectangle(5, 7, 7, 5, 1)
         self.assertEqual(5, r.width)
 
